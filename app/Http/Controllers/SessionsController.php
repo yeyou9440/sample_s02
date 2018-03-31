400f93ad2857a46b12c22a59a052b21a2ddefdfa
yeyou9440 <yeyou9440@163.com>
2018-03-31 21:36:49 +0800

    login logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Auth;
+
+class SessionsController extends Controller
+{
+    //
+    public function create()
+    {
+        return view('sessions.create');
+    }
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request,[
+          'email' => 'required|email|max:255',
+           'password' => 'required'
+        ]);
+        if(Auth::attempt($credentials,$request->has('remember'))){
+            session()->flash('success','welcome back~');
+            return redirect()->route('users.show',[Auth::user()]);
+        }
+        else {
+          session()->flash('danger','sorry wrong email or password~');
+          return redirect()->back();
+        }
+        return ;
+    }
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success','logout good');
+        return redirect('login');
+    }
+}
