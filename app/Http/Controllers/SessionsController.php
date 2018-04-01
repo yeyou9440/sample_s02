4cabcf59164805975e025cf5d9f4a74d1fbc93c4
yeyou9440 <yeyou9440@163.com>
2018-04-01 17:50:11 +0800

    delete - admin

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,6 +9,12 @@ use Auth;
 class SessionsController extends Controller
 {
     //
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
     public function create()
     {
         return view('sessions.create');
@@ -21,7 +27,7 @@ class SessionsController extends Controller
         ]);
         if(Auth::attempt($credentials,$request->has('remember'))){
             session()->flash('success','welcome back~');
-            return redirect()->route('users.show',[Auth::user()]);
+            return redirect()->intended(route('users.show',[Auth::user()]));
         }
         else {
           session()->flash('danger','sorry wrong email or password~');
